ca27574694e8902fd151cd1917742e298955a9ae
Dan Rouse <kremonte@gmail.com>
2014-11-12 20:12:52 -0500

    change TreeNode object/proto structure, work on configs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,170 +21,43 @@ function _svgelem(tag, initialAttrs) {
 
 // single node object
 // generates its own SVG recursively
-var TreeNode = function(nodeType, content, options, parent) {
-	this.type = nodeType;
+//var TreeNode = function(nodeType, content, options, parent) {
+var TreeNode = function(options) {
+	this.options = options.options;
+	this.parent = options.parent;
 	this.children = [];
-	this.content = content || '';
-	this.options = options;
-	this.parent = parent;
 
-	this.addChild = function(childType, childContent, position) {
-		var child = new TreeNode(childType, childContent, this.options, this);
-		position = position || this.children.length;
+	var fromString = options.fromString || '';
+
+	if(fromString.length) {
+		// generate from bracketed string
+		return this.fromString(fromString);
+	} else {
+		this.type = options.type;
+		this.head = options.head;
+	}
+}
+
+TreeNode.prototype = {
+	addChild: function(options) {
+		// allow just phrase name to be passed instead of options
+		if(typeof options === 'string') {
+			options = { type: options, head: '' };
+		}
+
+		var child = new TreeNode({ type: options.type, head: options.head, options: this.options, parent: this });
+		var position = options.position || this.children.length;
 
 		this.children.splice(position, 0, child);
 		return child;
-	};
+	},
 
-	this.removeChild = function(position) {
+	removeChild: function(position) {
 		this.children.splice(position, 1);
-	};
-
-	// convert this node and its children into bracketed text
-	this.toString = function() {
-		var out = '[' + this.type;
-
-		if(this.children.length) {
-			// recurse through children
-			for(var i in this.children) {
-				out += ' ' + this.children[i].toString();
-			}
-		} else if(this.content.length) {
-			// add head
-			out += ' ' + this.content;
-		}
-
-		return out + ']';
-	};
-
-	// create an SVG DOM element containing this node and all its children
-	this.toSVG = function(depth) {
-		depth = depth || 1;
-
-		var svg = _svgelem('svg'),
-			lines = [],
-			elemWidth = 0,
-			i;
-
-		// SVG must be rendered for getBBox to get size
-		// (? i'd like to remove this later but it doesn't
-		//  appear to be in the DOM?)
-		$('body').append(svg);
-
-		// add main label
-		var label = _svgelem('text', { 'class': 'sprouts__label' });
-		label.appendChild(document.createTextNode(this.type));
-		svg.appendChild(label);
-
-		var labelY = svg.getBBox().height;
-		var labelWidth = svg.getBBox().width;
-
-		// render children
-		if(this.children.length) {
-			var children = _svgelem('g', { 'class': 'children' });
-			svg.appendChild(children);
-
-			for(i in this.children) {
-				// Generate children recursively
-				var child = this.children[i].toSVG(depth + 1);
-				children.appendChild(child);
-				var childWidth = child.getBBox().width;
-
-				// Position the child adjacent to any existing siblings
-				child._attrs({
-					'x': elemWidth,
-					'y': options.nodeSpacingY
-				});
-
-				// create first point of connecting line at top center of child
-				var line = _svgelem('line');
-				line._attrs({
-					'stroke': options.lineColor,
-					'stroke-width': options.lineWidth,
-					'x1': elemWidth + (childWidth / 2),
-					'y1': options.nodeSpacingY - options.linePadding
-				});
-				lines.push(line);
-				svg.appendChild(line);
-
-				// re-center child + line if this is the only child
-				// and it's thinner than its parent
-				if(this.children.length === 1 && childWidth < labelWidth) {
-					child._attrs({ 'x': (labelWidth - childWidth) / 2 });
-					line._attrs({ 'x1': ((labelWidth - childWidth) / 2) + (childWidth / 2) })
-				}
-
-				// increase total width, add x-padding if not the last item
-				elemWidth += childWidth + (i < this.children.length - 1 ? options.nodeSpacingX : 0);
-			}
-
-		} else if(this.content.length) {
-			// no children, but there is a lexical head to display
-			var head = _svgelem('text', { 'class': 'sprouts__head' });
-			head.appendChild(document.createTextNode( this.content ));
-			svg.appendChild(head);
-		}
-
-		
-
-		// get total width (label + children) and center label
-		var bbox = svg.getBBox();
-		label._attrs({
-			'alignment-baseline': 'text-before-edge',
-			'text-anchor': 'middle',
-			'x': bbox.width / 2
-		});
-
-		// point lines towards center
-		for(i in lines) {
-			lines[i]._attrs({
-				'x2': bbox.width / 2,
-				'y2': labelY + options.linePadding
-			});
-		}
-
-		// move lexical head under label as well
-		if(head) {
-			head._attrs({
-				'alignment-baseline': 'hanging',
-				'y': labelY + 2 // 2px static y padding to avoid cutoff
-			});
-
-			// make the svg slightly taller to account for the 2 units of padding we need
-			// recalc bbox height for moved text
-			bbox.height = svg.getBBox().height + 2;
-		}
-
-		// explicitly state final size (we can scale later)
-		svg._attrs({
-			width: bbox.width,
-			height: bbox.height
-		});
-
-		// quick reference to node type for debugging
-		svg._nodeType = this.type;
-
-		return svg;
-	};
-};
-
-// tree master object
-// provides a root object, text parser, and writes the product to the DOM
-var Tree = function(contents, initialOptions) {
-	var options = this.options = initialOptions || {};
-
-	options.nodeSpacingY = typeof options.nodeSpacingY !== 'undefined' ? options.nodeSpacingY : 50;
-	options.nodeSpacingX = typeof options.nodeSpacingX !== 'undefined' ? options.nodeSpacingX : 12;
-	options.linePadding = typeof options.linePadding !== 'undefined' ? options.linePadding : 2;
-	options.lineWidth = typeof options.lineWidth !== 'undefined' ? options.lineWidth : 2;
-	options.lineColor = options.lineColor || '#333333';
-
-
-	this.treeElement = document.getElementById('sprouts-tree');
-	this.textElement = document.getElementById('sprouts-text');
+	},
 
 	// parses bracketed text into TreeNode objects
-	this.fromString = function(text) {
+	fromString: function(text) {
 		// match the phrase type and its contents
 		var currentObject,
 			currentPhrase = '',
@@ -198,8 +71,8 @@ var Tree = function(contents, initialOptions) {
 			   (text[i] === '[' || text[i] === ']' || text[i] === ' ')) {
 				// create root if we need to
 				var newObject = currentObject
-						? currentObject.addChild(currentPhrase, currentHead)
-						: new TreeNode(currentPhrase, currentHead, this.options);
+						? currentObject.addChild({ type: currentPhrase, head: currentHead })
+						: new TreeNode({ type: currentPhrase, head: currentHead, options: this.options });
 
 				currentObject = newObject;
 				currentPhrase = '';
@@ -212,13 +85,12 @@ var Tree = function(contents, initialOptions) {
 			} else if(text[i] === ']') {
 				// bracket close, apply any captured head
 				if(currentHead.length) {
-					currentObject.contents = currentHead;
+					currentObject.head = currentHead;
 					currentHead = '';
 				}
 
 				// ascend back up tree
 				if(currentObject.parent) {
-					currentObject
 					currentObject = currentObject.parent;
 				}
 
@@ -247,7 +119,179 @@ var Tree = function(contents, initialOptions) {
 		}
 		
 		return currentObject;
-	};
+	},
+
+	// convert this node and its children into bracketed text
+	toString: function() {
+		var out = '[' + this.type;
+
+		if(this.children.length) {
+			// recurse through children
+			for(var i in this.children) {
+				out += ' ' + this.children[i].toString();
+			}
+		} else if(this.head.length) {
+			// add head
+			out += ' ' + this.head;
+		}
+
+		return out + ']';
+	},
+
+	// create an SVG DOM element containing this node and all its children
+	toSVG: function(depth) {
+		depth = depth || 1;
+
+		var svg = _svgelem('svg'),
+			lines = [],
+			elemWidth = 0,
+			i;
+
+		// SVG must be rendered for getBBox to get size
+		// (? i'd like to remove this later but it doesn't
+		//  appear to be in the DOM?)
+		$('body').append(svg);
+
+		// add main label
+		var label = _svgelem('text', {
+			'class': 'sprouts__label',
+			'font-family': this.options.nodeFontFamily,
+			'font-size': this.options.nodeFontSize,
+			'color': this.options.nodeFontColor,
+			'font-weight': this.options.nodeFontBold ? 'bold' : 'normal',
+			'font-style': this.options.nodeFontItalic ? 'italic' : 'normal'
+		});
+		label.appendChild(document.createTextNode(this.type));
+		svg.appendChild(label);
+
+		var labelY = svg.getBBox().height;
+		var labelWidth = svg.getBBox().width;
+
+		// render children
+		if(this.children.length) {
+			var children = _svgelem('g', { 'class': 'children' });
+			svg.appendChild(children);
+
+			for(i in this.children) {
+				// Generate children recursively
+				var child = this.children[i].toSVG(depth + 1);
+				children.appendChild(child);
+				var childWidth = child.getBBox().width;
+
+				// Position the child adjacent to any existing siblings
+				child._attrs({
+					'x': elemWidth,
+					'y': this.options.nodeSpacingY
+				});
+
+				// create first point of connecting line at top center of child
+				var line = _svgelem('line');
+				line._attrs({
+					'class': 'sprouts__line',
+					'stroke': this.options.lineColor,
+					'stroke-width': this.options.lineWidth,
+					'x1': elemWidth + (childWidth / 2),
+					'y1': this.options.nodeSpacingY - this.options.linePadding
+				});
+				lines.push(line);
+				svg.appendChild(line);
+
+				// re-center child + line if this is the only child
+				// and it's thinner than its parent
+				if(this.children.length === 1 && childWidth < labelWidth) {
+					child._attrs({ 'x': (labelWidth - childWidth) / 2 });
+					line._attrs({ 'x1': ((labelWidth - childWidth) / 2) + (childWidth / 2) })
+				}
+
+				// increase total width, add x-padding if not the last item
+				elemWidth += childWidth + (i < this.children.length - 1 ? this.options.nodeSpacingX : 0);
+			}
+
+		} else if(this.head.length) {
+			// no children, but there is a lexical head to display
+			var head = _svgelem('text', {
+				'class': 'sprouts__head',
+				'font-family': this.options.headFontFamily,
+				'font-size': this.options.headFontSize,
+				'color': this.options.headFontColor,
+				'font-weight': this.options.headFontBold ? 'bold' : 'normal',
+				'font-style': this.options.headFontItalic ? 'italic' : 'normal'
+			});
+			head.appendChild(document.createTextNode( this.head ));
+			svg.appendChild(head);
+		}		
+
+		// get total width (label + children) and center label
+		var bbox = svg.getBBox();
+		label._attrs({
+			'alignment-baseline': 'text-before-edge',
+			'text-anchor': 'middle',
+			'x': bbox.width / 2
+		});
+
+		// point lines towards center
+		for(i in lines) {
+			lines[i]._attrs({
+				'x2': bbox.width / 2,
+				'y2': labelY + this.options.linePadding
+			});
+		}
+
+		// move lexical head under label as well
+		if(head) {
+			head._attrs({
+				'alignment-baseline': 'hanging',
+				'y': labelY + 2 // 2px static y padding to avoid cutoff
+			});
+
+			// make the svg slightly taller to account for the 2 units of padding we need
+			// recalc bbox height for moved text
+			bbox.height = svg.getBBox().height + 2;
+		}
+
+		// explicitly state final size (we can scale later)
+		svg._attrs({
+			width: bbox.width,
+			height: bbox.height
+		});
+
+		// quick reference to node type for debugging
+		svg._nodeType = this.type;
+
+		return svg;
+	}
+};
+
+// tree master object
+// provides a root object, text parser, and writes the product to the DOM
+var defaults = {
+	nodeFontFamily: 'Open Sans',
+	nodeFontSize: 16,
+	nodeFontColor: '#0000ff',
+	nodeFontBold: true,
+	nodeFontItalic: false,
+	headFontFamily: 'Times New Roman',
+	headFontSize: 10,
+	headFontColor: '#ff0000',
+	headFontBold: false,
+	headFontItalic: true,
+	nodeSpacingY: 50,
+	nodeSpacingX: 12,
+	linePadding: 2,
+	lineWidth: 2,
+	lineColor: '#333333'
+}
+var Tree = function(contents, initialOptions) {
+	var options = this.options = initialOptions || {};
+
+	for(var key in defaults) {
+		if(typeof options[key] === 'undefined') {
+			options[key] = defaults[key];
+		}
+	}
+
+	this.treeElement = document.getElementById('sprouts-tree');
+	this.textElement = document.getElementById('sprouts-text');
 
 	// converts internal Tree structure to bracketed text
 	this.toString = function() {
@@ -272,13 +316,11 @@ var Tree = function(contents, initialOptions) {
 	};
 
 	// initialize
-	if(contents.length) {
-		this.root = this.fromString(contents);
-	} else {
-		this.root = new TreeNode('XP', null, options);
-	}
+	this.root = new TreeNode({
+	 	fromString: contents,
+	 	options: options });
+
 	this.draw(options.noTextUpdate);
-	console.log('new tree', this.root);
 };
 
 
@@ -302,11 +344,21 @@ jQuery(function() {
 
 	// settings handlers
 	$('.setting').on('change', function(event) {
-		console.log('setting change', event);
+		// remove 'settings_' from name for options key
+		var value = this.value;
+		if(this.type === 'number' || this.type === 'range') {
+			value = parseFloat(value);
+		}
+		tree.options[this.name.substr(9)] = value;
+		tree.draw(true);
 	});
 
 	// databind the tree contents text input
 	$('#sprouts-text').on('keyup', function(event) {
-		tree = new Tree(this.value, { noTextUpdate: true });
+		tree.root = new TreeNode({
+		 	fromString: this.value,
+		 	options: tree.options });
+
+		tree.draw(true);
 	});
 });
