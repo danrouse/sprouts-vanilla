710d2be74fe9125a74d2d4aafccb6e26c70d73ab
Dan Rouse <kremonte@gmail.com>
2014-11-13 04:57:16 -0500

    fixed image not copying on gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -36,16 +36,16 @@ gulp.task('html', ['styles', 'scripts'], function () {
         .pipe($.size());
 });
 
-// gulp.task('images', function () {
-//     return gulp.src('app/images/**/*')
-//         .pipe($.cache($.imagemin({
-//             optimizationLevel: 3,
-//             progressive: true,
-//             interlaced: true
-//         })))
-//         .pipe(gulp.dest('dist/images'))
-//         .pipe($.size());
-// });
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        // .pipe($.cache($.imagemin({
+        //     optimizationLevel: 3,
+        //     progressive: true,
+        //     interlaced: true
+        // })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe($.size());
+});
 
 // gulp.task('fonts', function () {
 //     return $.bowerFiles()
@@ -64,7 +64,7 @@ gulp.task('clean', function () {
     return gulp.src(['.tmp', 'dist', 'docs'], { read: false }).pipe($.clean());
 });
 
-gulp.task('build', ['html', /*'images', 'fonts',*/ 'extras', 'docs']);
+gulp.task('build', ['html', 'images', /*'fonts',*/ 'extras', 'docs']);
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
@@ -115,14 +115,14 @@ gulp.task('watch', ['connect', 'serve'], function () {
         'app/*.html',
         '.tmp/styles/**/*.css',
         'app/scripts/**/*.js',
-        //'app/images/**/*'
+        'app/images/**/*'
     ]).on('change', function (file) {
         server.changed(file.path);
     });
 
     gulp.watch('app/styles/**/*.scss', ['styles']);
     gulp.watch('app/scripts/**/*.js', ['scripts']);
-    //gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('app/images/**/*', ['images']);
     gulp.watch('bower.json', ['wiredep']);
 });
 
