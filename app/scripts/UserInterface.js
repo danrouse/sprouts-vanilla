eb883d56fccb8eda582d3c16d8c3311d7d1d8535
Dan Rouse <kremonte@gmail.com>
2014-11-14 16:56:07 -0500

    UI refactoring work

---
+++ app/scripts/UserInterface.js
@@ -0,0 +1,499 @@
+'use strict';
+
+/**
+ * UI master object
+ * provides a root object and DOM interface
+ *
+ * @class UserInterface
+ * @constructor
+ * @param rootNode {TreeNode} root node of the tree
+ **/
+var UserInterface = function(rootNode) {
+    /**
+     * root node of the tree
+     *
+     * @property tree {TreeNode}
+     **/
+    this.tree = rootNode;
+
+    /**
+     * DOM element containing the tree SVG
+     *
+     * @property treeElement {Element}
+     **/
+    this.treeElement = document.getElementById('sprouts-tree');
+
+    /**
+     * DOM element containing the bracketed text representation of the tree
+     *
+     * @property treeElement {Element}
+     **/
+    this.textElement = document.getElementById('sprouts-text');
+
+    /**
+     * node to move. Next node selected will be target.
+     *
+     * @property nodeToMove {TreeNode}
+     **/
+    this.nodeToMove = null;
+
+    /**
+     * current selection
+     *
+     * @property selectedNode {TreeNode}
+     **/
+    this.selectedNode = null;
+
+    this.init();
+
+    // render
+    this.select(rootNode, true);
+};
+UserInterface.prototype = {
+    /**
+     * selects a node in the tree
+     *
+     * @method select
+     * @param targetNode {TreeNode}
+     * @param redrawTree {Boolean} redraw the tree
+     * @param redrawText {Boolean}
+     * @return targetNode {TreeNode} selected node
+     **/
+    select: function(targetNode, redrawTree, redrawText) {
+        if(redrawTree) { this.draw(); }
+
+        // remove existing selection class
+        if(this.selectedNode) {
+            this.selectedNode.svg._attrs({'class': ''});
+        }
+
+        // select target node
+        targetNode.svg._attrs({'class': 'selected'});
+        this.selectedNode = targetNode;
+
+        // update UI action buttons
+        // TODO: make a better system for this
+        if(targetNode.parent) {
+            document.getElementById('button-startMovement').style.display = 'block';
+        } else {
+            document.getElementById('button-startMovement').style.display = 'none';
+        }
+
+        // update selected text area
+        if(redrawText !== false) {
+            this.textElement.innerHTML = targetNode.toString(false, true);
+        }
+
+        return targetNode;
+    },
+
+    /**
+     * generates SVG and updates the DOM
+     *
+     * @method draw
+     **/
+    draw: function() {
+        // generate SVG
+        var svg = this.tree.toSVG();
+
+        // replace the element in the DOM
+        if(this.treeElement.children.length) {
+            this.treeElement.removeChild(this.treeElement.firstChild);
+        }
+        this.treeElement.appendChild(svg);
+
+        // reapply UI styles
+        if(this.nodeToMove) {
+            this.nodeToMove.svg._attrs({'class': 'moving'});
+        } else if(this.selectedNode) {
+            this.selectedNode.svg._attrs({'class': 'selected'});
+        }
+
+        // bind UI events
+        var that = this;
+        svg.onclick = function(event) {
+            // find first svg parent of click event target
+            var target = event.target;
+            while(target.nodeName !== 'svg') {
+                target = target.parentNode;
+            }
+
+            if(that.nodeToMove) {
+                that.select(that.nodeToMove.moveTo(target.treeNode), true);
+                that.nodeToMove = null;
+            } else {
+                that.select(target.treeNode);
+            }
+        };
+    },
+
+    init: function() {
+        var options = this.tree.options,
+            that = this;
+
+        // initialize the UI
+        // populate settings elements with defaults
+        var elem;
+        for(var key in options) {
+            if(key === 'fonts') { continue; }
+
+            elem = document.getElementsByName('settings_' + key);
+            if(elem.length) {
+                if(elem[0].type === 'checkbox') {
+                    elem[0].checked = options[key];
+                } else if(elem[0].nodeName === 'SELECT') {
+                    elem[0].querySelector('[value="' + options[key] + '"]').selected = true;
+                } else {
+                    elem[0].value = options[key];
+                }
+            } else {
+                console.log('no DOM element for setting', key);
+            }
+        }
+
+
+        // font selectors
+        var fonts = ['Open Sans', 'Arial', 'Times New Roman', 'Impact', 'Garamond', 'Comic Sans MS', 'Courier New'];
+        var fontSelectors = document.getElementsByClassName('font-selector'),
+            fontListTemplate = document.createElement('ul'),
+            i, j;
+
+        // populate template
+        for(i in fonts) {
+            var li = document.createElement('li');
+            li.setAttribute('style', 'font-family: ' + fonts[i]);
+            li.innerHTML = fonts[i];
+            fontListTemplate.appendChild(li);
+        }
+
+        // update preview and tree
+        var updateFont = function(fontPreview, destFont, attrs) {
+            // copy attrs to destination font
+            for(var key in attrs) {
+                destFont[key] = attrs[key];
+            }
+
+            // update preview
+            var style = 'font-family: ' + destFont.fontFamily +
+                ';font-size: ' + destFont.fontSize + 'pt' +
+                ';color: ' + destFont.color +
+                ';font-weight: ' + (destFont.bold ? 'bold' : 'normal') +
+                ';font-style: ' + (destFont.italic ? 'italic' : 'normal');
+            fontPreview.style.cssText = style;
+            fontPreview.innerHTML = destFont.fontFamily + ' ' +
+                destFont.fontSize + 'pt' +
+                (destFont.bold ? ' bold' : '') +
+                (destFont.italic ? ' italic' : '');
+
+            // redraw tree if changed
+            that.draw();
+        };
+
+        for(i=0; i<fontSelectors.length; i++) {
+            var selector = fontSelectors[i],
+                destFont = options.fonts[selector.dataset.font],
+                fontPreview = selector.getElementsByClassName('font-preview')[0],
+                fontList = selector.insertBefore(fontListTemplate.cloneNode(true), fontPreview);
+
+            // update font family on list item click
+            fontList.onclick = function() {
+                if(event.target.nodeName === 'LI') {
+                    updateFont(this.parentNode.getElementsByClassName('font-preview')[0],
+                        options.fonts[this.parentNode.dataset.font],
+                        { 'font-family': event.target.innerHTML });
+                }
+                // hide
+                this.className = '';
+            };
+
+            // show font list when preview is clicked
+            fontPreview.onclick = function() {
+                this.previousSibling.className = 'visible';
+            };
+
+            // hide the font list if we click out anywhere else
+            // document.onclick = function(e) {
+            //     if(fontListVisible &&
+            //        event.target.parentNode !== fontList &&
+            //        event.target.parentNode !== fontPreview &&
+            //        event.target.parentNode !== selector) {
+            //         // this is conflicting with multiple elements
+            //         // (multiple document.onclick functions hiding each other)
+
+            //         // fontList.className = '';
+            //         // console.log('hiding', event.target.parentNode, 'selector', selector);
+            //         // fontListVisible = false;
+            //     }
+            // }
+
+            // simple event handlers for the chirrens
+            selector.children.fontSize.oninput = function() {
+                updateFont(this.parentNode.getElementsByClassName('font-preview')[0],
+                    options.fonts[this.parentNode.dataset.font],
+                    { fontSize: this.value });
+            };
+            selector.children.color.onchange = function() {
+                updateFont(this.parentNode.getElementsByClassName('font-preview')[0],
+                    options.fonts[this.parentNode.dataset.font],
+                    { color: this.value });
+            };
+            selector.children.bold.onchange = function() {
+                updateFont(this.parentNode.getElementsByClassName('font-preview')[0],
+                    options.fonts[this.parentNode.dataset.font],
+                    { bold: this.checked });
+            };
+            selector.children.italic.onchange = function() {
+                updateFont(this.parentNode.getElementsByClassName('font-preview')[0],
+                    options.fonts[this.parentNode.dataset.font],
+                    { italic: this.checked });
+            };
+
+            // initialize
+            selector.children.fontSize.value = destFont.fontSize;
+            selector.children.color.value = destFont.color;
+            selector.children.bold.checked = destFont.bold;
+            selector.children.italic.checked = destFont.italic;
+            updateFont(fontPreview, destFont);
+        }
+
+        // non-font settings handlers
+        function changeSetting() {
+            var val = this.value;
+            if(this.type === 'number' || this.type === 'range') {
+                val = parseFloat(val);
+            } else if(this.type === 'checkbox') {
+                val = this.checked;
+            }
+            options[this.name.substr(9)] = val;
+            this.draw();
+        }
+        var settingElems = document.getElementsByClassName('setting');
+        for(var i=0; i<settingElems.length; i++) {
+            settingElems[i].onchange = changeSetting.bind(settingElems[i]);
+        }
+
+        // collapsible settings groups
+        function toggleCollapsible() {
+            // dataset won't hold true booleans
+            if(this.dataset.collapsed !== 'true') {
+                console.log('hiding');
+                this.nextElementSibling.style.display = 'none';
+                this.dataset.collapsed = 'true';
+                this.firstChild.firstChild.setAttribute('xlink:href', '#icon-plus');
+            } else {
+                console.log('showing');
+                this.nextElementSibling.style.display = 'block';
+                this.dataset.collapsed = false;
+                this.firstChild.firstChild.setAttribute('xlink:href', '#icon-minus');
+            }
+        }
+        var collapsibleElems = document.getElementsByClassName('collapsible');
+        for(var i=0; i<collapsibleElems.length; i++) {
+            collapsibleElems[i].onclick = toggleCollapsible.bind(collapsibleElems[i]);
+        }
+
+        // button click handlers
+        var actions = {
+            toggleSidebar: function(event, target) {
+                var sidebar = document.getElementById('sidebar');
+                if(sidebar.className === 'sidebar collapsed') {
+                    sidebar.className = 'sidebar';
+                    target.firstElementChild.firstElementChild.setAttribute('xlink:href', '#icon-left');
+                } else {
+                    sidebar.className = 'sidebar collapsed';
+                    target.firstElementChild.firstElementChild.setAttribute('xlink:href', '#icon-right');
+                }
+            },
+
+            addChild: function() {
+                var child = that.selectedNode.addChild('XP');
+                that.select(child, true);
+            },
+
+            removeNode: function() {
+                var newSelection = that.selectedNode.parent;
+                for(var i in newSelection.children) {
+                    if(newSelection.children[i] === that.selectedNode) {
+                        newSelection.children.splice(i, 1);
+                        break;
+                    }
+                }
+
+                that.select(newSelection, true);
+            },
+
+            startMovement: function() {
+                var target = that.selectedNode;
+                if(!that.nodeToMove) {
+                    that.nodeToMove = target;
+                    target.svg._attrs({ 'class': 'moving' });
+                } else {
+                    that.nodeToMove = null;
+                    target.svg._attrs({ 'class': 'selected' });
+                }
+            },
+
+            saveSVG: function() {
+                var slug = that.toString(true),
+                    svg = that.tree.svg,
+                    xml = '<svg xmlns="http://www.w3.org/2000/svg" width="' + svg.offsetWidth +
+                        '" height="' + svg.offsetHeight + '">' + svg.innerHTML + '</svg>';
+
+                // make slug filename-friendly
+                slug = slug.toLowerCase().replace(/\s+/g, '-').substr(0,32);
+
+                _download('data:image/svg+xml,' + encodeURIComponent(xml), 'sprouts-' + slug + '.svg');
+            },
+
+            savePNG: function() {
+                var slug = that.toString(true);
+
+                // make slug filename-friendly
+                slug = slug.toLowerCase().replace(/\s+/g, '-').substr(0,32);
+
+                _download(that.tree.toPNG(), 'sprouts-' + slug + '.png');
+            }
+        };
+        function handleAction(event) {
+            var target = event.target;
+            while(target.parentNode && target.nodeName !== 'BUTTON') {
+                target = target.parentNode;
+            }
+            // get target action
+            var action = actions[target.dataset.action];
+            if(action) { action(event, target); }
+        }
+        var buttonElems = document.getElementsByTagName('button');
+        for(var i=0; i<buttonElems.length; i++) {
+            buttonElems[i].onclick = handleAction;
+        }
+
+        // databind the tree contents text input
+        function updateText(event) {
+            // check if text was really updated
+            if(this.lastText &&
+               this.lastText === this.innerText &&
+               event.type !== 'blur') {
+                return;
+            }
+            var parent = that.selectedNode.parent;
+
+            // create new node from contents
+            var newNode = new TreeNode({
+                fromString: this.innerText,
+                options: options
+            });
+
+            if(parent) {
+                // replace reference in the parent
+                for(var i in parent.children) {
+                    if(parent.children[i] === that.selectedNode) {
+                        parent.children[i] = newNode;
+                    }
+                }
+
+                // reference parent in the new node
+                newNode.parent = parent;
+            } else {
+                // replace root
+                that.tree = newNode;
+            }
+
+            // update selection and redraw
+            that.select(newNode, true, (event.type === 'blur' || event.type === 'paste'));
+            // var selection = window.getSelection(),
+            //     rangeBefore = selection.getRangeAt(0).startOffset;
+            // tree.select(newNode, true, false);
+            // var rangeAfter = selection.getRangeAt(0).startOffset;
+            // var targetTextNode = this.childNodes[rangeAfter],
+            //     targetRange = document.createRange();
+            // console.log('target node offset', rangeBefore, rangeAfter, targetTextNode);
+            // targetRange.selectNodeContents(targetTextNode);
+            // console.log('new range', targetTextNode.nodeType);
+            // //targetRange.setStart(targetTextNode, rangeBefore);
+            // targetRange.collapse(true);
+            // selection.removeAllRanges();
+            // selection.addRange(targetRange);
+
+            this.lastText = this.innerText;
+        }
+        this.textElement.onkeyup = updateText;
+        this.textElement.onblur = updateText;
+        this.textElement.onpaste = updateText;
+        this.textElement.oninput = updateText;
+
+        // keyboard controls
+        document.onkeydown = function(event) {
+            if(event.target.nodeName !== 'BODY') { return; }
+
+            var selectedNode = that.selectedNode,
+                parent = selectedNode.parent,
+                siblings = parent ? parent.children : null;
+
+            switch(event.keyCode) {
+                case 37:
+                    // left arrow: select sibling to the left
+                    var target;
+
+                    if(siblings) {
+                        var index = siblings.indexOf(that.selectedNode);
+                        if(index > 0) {
+                            target = siblings[index - 1];
+                        }
+                    }
+                    if(!target) {
+                        // select first child
+                        if(selectedNode.children.length) {
+                            target = selectedNode.children[0];
+                        }
+                    }
+                    
+                    if(target) { that.select(target); }
+                    break;
+
+                case 38:
+                    // up arrow: select parent
+                    if(selectedNode.parent) {
+                        that.select(selectedNode.parent);
+                    }
+                    break;
+
+                case 39:
+                    // right arrow: select sibling to the right
+                    var target;
+                    
+                    if(siblings) {
+                        var index = siblings.indexOf(that.selectedNode);
+                        if(index < siblings.length - 1) {
+                            target = siblings[index + 1];
+                        }
+                    }
+                    if(!target) {
+                        // select last child
+                        if(selectedNode.children.length) {
+                            target = selectedNode.children[selectedNode.children.length - 1];
+                        }
+                    }
+                    
+                    if(target) { that.select(target); }
+                    break;
+
+                case 40:
+                    // down arrow: select first child
+                    if(selectedNode.children.length) {
+                        var index = Math.floor(selectedNode.children.length / 2) - 1;
+                        that.select(selectedNode.children[index]);
+                    }
+                    break;
+
+                case 13:
+                    // enter: create child
+                    that.select(selectedNode.addChild('XP'), true);
+                    break;
+
+                default:
+                    console.log('keycode', event.keyCode);
+            }
+        };
+    }
+};
